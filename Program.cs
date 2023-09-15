ee830b867912c6d9136991635bd58b95f2304950
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-14 20:26:42 -0500

    metodosCcase 10

--- Program.cs
+++ Program.cs
@@ -1,112 +1,201 @@
-﻿namespace Tecnicasdeprogramacion1
+﻿ using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace Tecnicasdeprogramacion1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-         Console.WriteLine("Por favor, ingresa un opción:");
-         Console.WriteLine("Ejercicio 1:Diseñar un algoritmo que pida un número por teclado y luego imprima el número siguiente al ingresado.");
-         Console.WriteLine("Ejercicio 2:Diseñar un algoritmo que genere un número aleatorio del 0 al 200,lo muestre y luego calcule y muestre el mismo número aumentado en un 30 %.");
-         Console.WriteLine("Ejercicio 3: Diseñar un algoritmo que imprima el área y el perímetro de un rectángulo ingresando su base y altura.");
-         Console.WriteLine("Ejercicio 4: Realice un algoritmo que calcule el volumen de un cilindro a partir de los valores de su radio y altura.");
-         Console.WriteLine("Ejercicio 5: Crear un algoritmo que convierta y muestre un valor ingresado en centímetros a yardas, metros, pies y pulgadas.");
-         Console.WriteLine("Ejercicio 6: Diseñar un algoritmo que convierta y muestre la temperatura en Fahrenheit ingresando la temperatura en Celsius.");
-         Console.WriteLine("Ejercicio 7: Diseñar un algoritmo que calcule el volumen de un cilindro dados su radio y altura(primero el programa deberá verificar si son positivas).");
-         Console.WriteLine("Ejercicio 2:Diseñar un algoritmo que genere un número aleatorio del 0 al 200,lo muestre y luego calcule y muestre el mismo número aumentado en un 30 %.");
-         Console.WriteLine("Ejercicio 2:.");
-         Console.WriteLine("10:Salir.");
+            Console.WriteLine("Por favor, ingresa una opción:");
+            Console.WriteLine("1: Diseñar un algoritmo que pida un número por teclado y luego imprima el número siguiente al ingresado.");
+            Console.WriteLine("2: Generar un número aleatorio del 0 al 200, mostrarlo y luego calcular y mostrar el mismo número aumentado en un 30 %.");
+            Console.WriteLine("3: Calcular el área y el perímetro de un rectángulo ingresando su base y altura.");
+            Console.WriteLine("4: Calcular el volumen de un cilindro a partir de los valores de su radio y altura.");
+            Console.WriteLine("5: Convertir un valor ingresado en centímetros a yardas, metros, pies y pulgadas.");
+            Console.WriteLine("6: Convertir la temperatura en Fahrenheit ingresando la temperatura en Celsius.");
+            Console.WriteLine("7: Calcular el volumen de un cilindro dados su radio y altura (verificando si son positivas).");
+            Console.WriteLine("8: Crear un algoritmo que calcule si dos números son divisibles.");
+            Console.WriteLine("9: Diseñar un algoritmo para calcular el porcentaje de hombres y de mujeres que hay en un grupo, dados los totales de hombres y de mujeres.");
+            Console.WriteLine("10: Salir");
+
             int numero = int.Parse(Console.ReadLine());
-         switch (numero)
-         {  
-            case 1:
-                    Console.WriteLine("Digite un numero");
-                    string dato = Console.ReadLine();
-                    int num = Int32.Parse(dato);
-                    int num2 = num + 1;
-                    Console.WriteLine("El numero es: " + num2);
-                break;
-            case 2:
-                    Console.WriteLine("Generando un número al azar entre 0 a 200.");
-                    Random random = new Random();
-                    int numAleatorio = random.Next(0, 201);
-                    Console.WriteLine("El numero es aleatorio es: " + numAleatorio);
-                    double treintapor = 0.3 * numAleatorio;
-                    Console.WriteLine("El 30% del numero es aleatorio es: " + treintapor);
-                break;
-            case 3:
-                    Console.WriteLine("Datos para la calcular el area y el perimetro de un rectangulo.");
-                    Console.WriteLine("Digite la base:");
-                    string base1 = Console.ReadLine();
-                    int baseCu = Int32.Parse(base1);
-                    Console.WriteLine("Digite la altura:");
-                    string alt1 = Console.ReadLine();
-                    int altCu = Int32.Parse(alt1);
-                    int areaCu = baseCu * altCu;
-                    int perimetroCu = (2 * baseCu) + (2 * altCu);
-                    Console.WriteLine("El area de rectangulo es: " + areaCu + " y El perimetro es:" + perimetroCu);
-                break;
-            case 4:
-                    Console.WriteLine("Datos para la calcular el volumen de un cilindro.");
-                    Console.WriteLine("Digite la altura:");
-                    string alt2 = Console.ReadLine();
-                    int altCi = Int32.Parse(alt2);
-                    Console.WriteLine("Digite el radio:");
-                    string rad1 = Console.ReadLine();
-                    int radCi = Int32.Parse(rad1);
-                    double volumenCi = Math.PI * (Math.Pow(radCi, 2)) * altCi;
-                    Console.WriteLine("El volumen del cilindro es: " + volumenCi);
-                break;
-            case 5:
-                    Console.WriteLine("Conversor de unidades.");
-                    Console.WriteLine("Digite los centimetros a convertir:");
-                    string centimetroUsu = Console.ReadLine();
-                    double centimetros1 = Int32.Parse(centimetroUsu);
-                    double yardas = centimetros1 * 0.010936133;
-                    double metros = centimetros1 * 0.01;
-                    double pies = centimetros1 * 0.0328083989501;
-                    double pulgasdas = centimetros1 * 0.393700787402;
-                    Console.WriteLine("El valor en yardas es: " + yardas);
-                    Console.WriteLine("El valor en metros es: " + metros);
-                    Console.WriteLine("El valor en pies es: " + pies);
-                    Console.WriteLine("El valor en pulgadas es: " + pulgasdas);
-                break;
-            case 6:
-                    Console.WriteLine("Conversor de celsius a Fahrenheit.");
-                    Console.WriteLine("Digite los grados celsius a convertir:");
-                    string celsiusUsu = Console.ReadLine();
-                    double celsius1 = Int32.Parse(celsiusUsu);
-                    double fahren1 = ((celsius1 * 9) / 5) + 32; //(0 °C × 9/5) + 32 = 32 °F
-                    Console.WriteLine("El valor en grados Fahrenheit es: " + fahren1);
+
+            switch (numero)
+            {
+                case 1:
+                    SumaNumero();
                     break;
-            case 7:
-                   Console.WriteLine("Datos para calcular el volumen de un cilindro.");
-                   Console.WriteLine("Digite la altura:");
-                   string alt3 = Console.ReadLine();
-                   int altCi1 = Int32.Parse(alt3);
-                    if (altCi1 < 0)
-                    {
-                    Console.WriteLine("El número digitado no es Positivo");
-                    Console.WriteLine("Digite nuevamente la altura:");
-                    string alt4 = Console.ReadLine();
-                    altCi1 = Int32.Parse(alt4);
-                    }
-                    else
-                    {
-                    Console.WriteLine("Digite el radio:");
-                    string rad2 = Console.ReadLine();
-                    int radCi1 = Int32.Parse(rad2);
-                    if (radCi1 < 0)
-                    {
-                        Console.WriteLine("El número digitado no es Positivo");
-                        Console.WriteLine("Digite nuevamente el radio:");
-                        string rad3 = Console.ReadLine();
-                        radCi1 = Int32.Parse(rad3);
-                        }
-                        double volumenCi1 = Math.PI * Math.Pow(radCi1, 2) * altCi1;
-                        Console.WriteLine("El volumen del cilindro es: " + volumenCi1);
-                    } 
-                 break;
+                case 2:
+                    NumeroRamdon();
+                    break;
+
+                case 3:
+                    AreaRectangulo();
+                    break;
+
+                case 4:
+                    AltCilindro();
+                    break;
+
+                case 5:
+                    ConvertirCm();
+                    break;
+
+                case 6:
+                    ConvertirTemp(); 
+                    break;
+
+                case 7:
+                    AltCilindro1();
+                    break;
+
+                case 8:
+                    Divisible();
+                    break;
+                case 9:
+                    HombreMujer(); 
+                    break;
+                case 10:
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
             }
+
+        }
+        public static void SumaNumero() {
+            Console.WriteLine("Digite un número:");
+            int dato = int.Parse(Console.ReadLine());
+            int siguienteNumero = dato + 1;
+            Console.WriteLine("El número siguiente es: " + siguienteNumero);
+        }
+        public static void NumeroRamdon()
+        {
+            Random random = new Random();
+            int numAleatorio = random.Next(0, 201);
+            Console.WriteLine("Número aleatorio generado: " + numAleatorio);
+            double treintaPorciento = 0.3 * numAleatorio;
+            double nuevoNumero = numAleatorio + treintaPorciento;
+            Console.WriteLine("Número aumentado en un 30%: " + nuevoNumero);
+        }
+        public static void AreaRectangulo()
+        {
+            Console.WriteLine("Por favor, ingresa la base del rectángulo:");
+            int baseRectangulo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Por favor, ingresa la altura del rectángulo:");
+            int alturaRectangulo = int.Parse(Console.ReadLine());
+            int areaRectangulo = baseRectangulo * alturaRectangulo;
+            int perimetroRectangulo = 2 * (baseRectangulo + alturaRectangulo);
+            Console.WriteLine("El área del rectángulo es: " + areaRectangulo);
+            Console.WriteLine("El perímetro del rectángulo es: " + perimetroRectangulo);
+        }
+        public static void AltCilindro()
+        {
+            Console.WriteLine("Por favor, ingresa la altura del cilindro:");
+            int alturaCilindro = int.Parse(Console.ReadLine());
+            if (alturaCilindro < 0)
+            {
+                Console.WriteLine("La altura no puede ser negativa.");
+                return;
+            }
+
+            Console.WriteLine("Por favor, ingresa el radio del cilindro:");
+            int radioCilindro = int.Parse(Console.ReadLine());
+            if (radioCilindro < 0)
+            {
+                Console.WriteLine("El radio no puede ser negativo.");
+                return;
+            }
+
+            double volumenCilindro = Math.PI * Math.Pow(radioCilindro, 2) * alturaCilindro;
+            Console.WriteLine("El volumen del cilindro es: " + volumenCilindro); ;
+        }
+        public static string ConvertirCm()
+        {
+            Console.WriteLine("Por favor, ingresa la longitud en centímetros:");
+            double longitudCm = double.Parse(Console.ReadLine());
+            double yardas = longitudCm / 91.44;
+            double metros = longitudCm / 100;
+            double pies = longitudCm / 30.48;
+            double pulgadas = longitudCm / 2.54;
+
+            Console.WriteLine("Longitud en yardas: " + yardas);
+            Console.WriteLine("Longitud en metros: " + metros);
+            Console.WriteLine("Longitud en pies: " + pies);
+            Console.WriteLine("Longitud en pulgadas: " + pulgadas);
+            return;
+        }
+        public static void ConvertirTemp()
+        {
+            Console.WriteLine("Por favor, ingresa la temperatura en grados Celsius:");
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = ((celsius * 9) / 5) + 32;
+            Console.WriteLine("La temperatura en grados Fahrenheit es: " + fahrenheit);
+        }
+        public static void AltCilindro1()
+        {
+            Console.WriteLine("Por favor, ingresa la altura del cilindro:");
+            int alturaCilindro7 = int.Parse(Console.ReadLine());
+            if (alturaCilindro7 < 0)
+            {
+                Console.WriteLine("La altura no puede ser negativa.");
+                return;
+            }
+            Console.WriteLine("Por favor, ingresa el radio del cilindro:");
+            int radioCilindro7 = int.Parse(Console.ReadLine());
+            if (radioCilindro7 < 0)
+            {
+                Console.WriteLine("El radio no puede ser negativo.");
+                return;
+            }
+
+            double volumenCilindro7 = Math.PI * Math.Pow(radioCilindro7, 2) * alturaCilindro7;
+            Console.WriteLine("El volumen del cilindro es: " + volumenCilindro7);
+            return;
+        }
+        public static void Divisible()
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número:");
+            int numero2 = int.Parse(Console.ReadLine());
+            if (numero2 == 0)
+            {
+                Console.WriteLine("No se puede dividir por cero.");
+            }
+            else if (numero1 % numero2 == 0)
+            {
+                Console.WriteLine($"{numero1} es divisible por {numero2}");
+            }
+            else
+            {
+                Console.WriteLine($"{numero1} no es divisible por {numero2}");
+            }
+        }
+        public static void HombreMujer()
+        {
+            Console.WriteLine("Ingrese el número total de hombres en el grupo:");
+            int hombres = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número total de mujeres en el grupo:");
+            int mujeres = int.Parse(Console.ReadLine());
+
+            int totalPersonas = hombres + mujeres;
+
+            if (totalPersonas > 0)
+            {
+                double porcentajeHombres = (double)hombres / totalPersonas * 100;
+                double porcentajeMujeres = (double)mujeres / totalPersonas * 100;
+
+                Console.WriteLine($"En el grupo, el {porcentajeHombres}% son hombres y el {porcentajeMujeres}% son mujeres.");
+            }
+            else
+            {
+                Console.WriteLine("No se puede calcular el porcentaje sin datos válidos.");
+            }
+            return;
 
         }
     }
